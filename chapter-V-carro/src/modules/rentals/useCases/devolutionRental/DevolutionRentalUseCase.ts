2a03e2b86dbc160afdee0889c6f9a680544e9c24
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-08-10 15:21:08 -0300

    Carro - Listagem de Alugueis do Usuário

--- chapter-V-carro/src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ chapter-V-carro/src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
@@ -11,6 +11,7 @@ interface IRequest {
   user_id: string
 }
 
+@injectable()
 class DevolutionRentalUseCase {
   constructor(
     @inject("RentalsRepository")
@@ -23,7 +24,7 @@ class DevolutionRentalUseCase {
 
   async execute({ id, user_id }: IRequest): Promise<Rental> {
     const rental = await this.rentalsRepository.findById(id);
-    const car = await this.carsRepository.findById(id);
+    const car = await this.carsRepository.findById(rental.car_id);
 
     const minimum_daily = 1;
 
@@ -41,8 +42,8 @@ class DevolutionRentalUseCase {
     }
 
     const delay = this.dateProvider.compareInDays(
+      rental.expected_return_date,
       dateNow,
-      rental.expected_return_date
     );
 
     let total = 0;
