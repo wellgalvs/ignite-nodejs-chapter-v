7d00da455ef0b2c646cfa5e382ec2aefbe6c4530
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-08-16 09:13:09 -0300

    Recuperação de Senha - Inserindo Template Engine para Envio de E-mail

--- chapter-V-recuperacao-de-senha/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ chapter-V-recuperacao-de-senha/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -5,6 +5,7 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepositor
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -14,10 +15,12 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokensRepository")
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) { }
 
-  async execute(email: string) {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) throw new AppError("User does not exists!");
@@ -31,6 +34,12 @@ class SendForgotPasswordMailUseCase {
       expires_date,
       refresh_token: token
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de Senha",
+      `O link para o reset é ${token}`
+    );
   }
 }
 
